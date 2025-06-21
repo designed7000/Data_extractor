eaccbd8d534bdc827a343f10e23514bd273b0eac
designed7000 <alexander.tux@gmail.com>
2025-06-21 01:08:00 +0100

    final version

--- docs/aws_deployment.md
+++
@@ -1,5 +0,0 @@
-# Make it executable
-chmod +x manage_price_tracker.sh
-
-# Run the interactive menu
-./manage_price_tracker.sh
--- src/local/vis.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-dt = pd.read_csv('../data/products_monitors_20250611_141640.csv')
-
-print(dt['price_text'])
